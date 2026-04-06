be52cfa4a6e17efc1233184cbe41293b79ba60aa
mangeshaundhekar <mangeshaundhekar7@gmail.com>
2026-04-06 14:49:32 +0530

    Removed Login page

--- StudentCRUD/Program.cs
+++ StudentCRUD/Program.cs
@@ -19,6 +19,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Account}/{action=Login}/{id?}");
+    pattern: "{controller=Student}/{action=Index}/{id?}");
 
 app.Run();
